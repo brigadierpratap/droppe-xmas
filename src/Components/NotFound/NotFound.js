fbb2547fbd4a3c8b1972bd12818a88cd782e253b
Pawan Singh <pmarsingh@gmail.com>
2021-07-14 22:36:06 +0530

    Added Footer and Not Found

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "./NotFound.css";
+import notFound from "../../Assets/not-found.svg";
 function NotFound() {
-  return <div>nit fo</div>;
+  return (
+    <div className="not-found-container">
+      <div className="nf-inner">
+        <img src={notFound} alt="not-found" />
+        <span>Unexpected Error!</span>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
